50388f994c294f799b7a24b4ee0f411224408579
Pavel Novikov <spl.novikov@gmail.com>
2024-01-18 19:47:21 +0300

    checking sonarlint

--- src/ui/Auth/Auth.tsx
+++ src/ui/Auth/Auth.tsx
@@ -33,7 +33,7 @@ export function Auth(): ReactElement {
       </label>
 
       <button type="submit" disabled={loading}>
-        {loading ? 'Trying to login...' : 'Login'}
+        {loading ? 'Trying to login.....' : 'Login'}
       </button>
     </form>
   );
