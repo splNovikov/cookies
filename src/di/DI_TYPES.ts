4e1ec8927fcedf47659b7d99d129fa68442b6e7b
Pavel Novikov <spl.novikov@gmail.com>
2024-02-01 12:44:39 +0300

    removes unnecessary DI_TYPE

--- src/di/DI_TYPES.ts
+++ src/di/DI_TYPES.ts
@@ -1,10 +1,6 @@
 const DI_TYPES = {
-  // interfaces:
   NotificationService: Symbol.for('NotificationService'),
   ApiClientService: Symbol.for('ApiClientService'),
-
-  // self - classes:
-  OrderAppService: Symbol.for('OrderAppService'),
 };
 
 export { DI_TYPES };
