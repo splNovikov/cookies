a8bcf1553aa05aaa731438015a79370a068247e6
Pavel Novikov <spl.novikov@gmail.com>
2024-02-09 10:58:03 +0300

    removes commented code (forgot to uncomment)

--- src/primaryAdapters/userPrimaryAdapter.ts
+++ src/primaryAdapters/userPrimaryAdapter.ts
@@ -10,11 +10,11 @@ interface UserPrimaryAdapter {
 }
 export function useUserPrimaryAdapter(): UserPrimaryAdapter {
   const userAppService = diContainer.get(UserAppService);
-  // const [user, setUser] = useState(userAppService.getUser());
-  //
-  // userAppService.subscribe(() => {
-  //   setUser(userAppService.getUser());
-  // });
+  const [user, setUser] = useState(userAppService.getUser());
 
-  return { user: userAppService.getUser() };
+  userAppService.subscribe(() => {
+    setUser(userAppService.getUser());
+  });
+
+  return { user };
 }
