afebea5c7e941d3e0ee00dde2b534217fab1edf1
Pavel Novikov <spl.novikov@gmail.com>
2024-02-12 19:39:37 +0300

    lost this file during last commit

--- src/ui/Cookie/Toppings.tsx
+++ src/ui/Cookie/Toppings.tsx
@@ -3,21 +3,22 @@ import React, { Fragment, ReactElement } from 'react';
 import { useUserPrimaryAdapter } from '../../primaryAdapters/userPrimaryAdapter';
 import { Product } from '../../core/domain/model/Product';
 import { ingredients } from '../../core/domain/model/Ingredient';
-import { hasAllergy, hasPreference } from '../../core/domain/services/user';
+// todo: domain???
+import { hasPreference } from '../../core/domain/services/user';
 
 type ToppingsProps = {
   cookie: Product;
 };
 
 export function Toppings({ cookie }: Readonly<ToppingsProps>): ReactElement {
-  const { user } = useUserPrimaryAdapter();
+  const { user, hasAllergy } = useUserPrimaryAdapter();
 
   return (
     <ul>
       {cookie.toppings.map((topping) => (
         <li key={topping}>
           {ingredients[topping]} {!!user && hasPreference(user, topping) && <Fragment>👍</Fragment>}{' '}
-          {!!user && hasAllergy(user, topping) && <Fragment>⚠️</Fragment>}
+          {!!user && hasAllergy(topping) && <Fragment>⚠️</Fragment>}
         </li>
       ))}
     </ul>
